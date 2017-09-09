75efcfb3d701fc707f238169808e7ba96cb446ea
Polisonik <allred@inbox.ru>
2017-09-09 19:32:19 +0300

    Добавление сброса значений фильтров

--- js/form.js
+++ js/form.js
@@ -10,9 +10,10 @@
   var blockEffect = uploadOverlay.querySelector('.upload-effect-controls');
   var uploadResize = uploadOverlay.querySelector('.upload-resize-controls');
   var photo = uploadOverlay.querySelector('.effect-image-preview');
-
+  var toggle = document.querySelector('.upload-effect-level-pin');
+	var bar =document.querySelector('.upload-effect-level-val');
+	
   uploadFile.addEventListener('change', onInputFile);
-
   function onInputFile() {
     openUploadOverlay();
   }
@@ -25,6 +26,8 @@
     uploadCansel.addEventListener('click', onClickCansel);
     uploadCansel.addEventListener('keydown', onKeydownEnterCansel);
     form.addEventListener('submit', onSubmit);
+		toggle.style.display = 'none';
+		bar.style.width = '0%';			
   }
   function closeUploadOverlay() {
     uploadOverlay.classList.add('hidden');
@@ -37,6 +40,8 @@
     buttonIncrease.removeEventListener('keydown', onKeydownEnterIncrease);
     blockEffect.removeEventListener('click', onClickEffect);
     form.removeEventListener('submit', onSubmit);
+		uploadOverlay.removeEventListener('mousemove', onMouseMove);
+		uploadOverlay.removeEventListener('mouseup', onMouseUp);
   }
   function onSubmit(event) {
     window.validate.isValid(event, form);
@@ -127,16 +132,44 @@
     var target = event.target;
     var filterName = target.value;
     var defaultClass = 'effect-image-preview';
-    if (target.tagName !== 'INPUT') {
+		
+    var effectByDefault ={
+			none: 'none',
+			chrome: 'grayscale(1)',
+			sepia: 'sepia(1)',
+			marvin: 'invert(100%)',
+			phobos: 'blur(5px)',
+			heat: 'brightness(3)'
+		}
+
+		if (target.tagName !== 'INPUT') {
       return;
     }
     photo.className = defaultClass + ' ' + 'effect-' + filterName;
+		toggle.style.left = '20%';
+		toggle.style.display = 'block';
+		bar.style.width = '20%';
+		if (photo.classList.contains('effect-chrome')) {
+					photo.style.filter = effectByDefault.chrome;
+				} else if (photo.classList.contains('effect-sepia')) {
+					photo.style.filter = effectByDefault.sepia;
+				} else if (photo.classList.contains('effect-marvin')) {
+					photo.style.filter = effectByDefault.marvin;
+				} else if (photo.classList.contains('effect-phobos')) {
+					photo.style.filter = effectByDefault.phobos;
+			  } else if (photo.classList.contains('effect-heat')) {
+					photo.style.filter = effectByDefault.heat;
+				} else if(photo.classList.contains('effect-none') || photo.className === defaultClass ) {
+					photo.style.filter = effectByDefault.none;
+					toggle.style.display = 'none';
+		      bar.style.width = '0%';
+				}
   }
 	// Оживление ползунка
 	//1. <div class="upload-effect-level-line"> - шкала длина 455 px
 	//2. <div class="upload-effect-level-pin"></div>  - 18px X 18px ползунок
 	//3. <div class="upload-effect-level-val"></div> - линия заполнения, 20% начальное значение.
-	//4. Для фильтра «Хром» — filter: grayscale(0..1);
+
 	function moveToggle() {
 		
 		var toggle = document.querySelector('.upload-effect-level-pin');
@@ -150,45 +183,43 @@
 			event.preventDefault();
 			var startX = event.clientX;
 			var widthToggle = toggle.clientWidth;
-		  var widthLine = line.offsetWidth;
-											
+		  										
 			document.addEventListener('mousemove', onMouseMove);
 			document.addEventListener('mouseup', onMouseUp);
 			
 			function onMouseMove(moveEvent) {
 				moveEvent.preventDefault();
-
-				var shiftX = startX - moveEvent.clientX;
-				var leftLimit = line.offsetLeft - widthToggle;
-				var rightLimit = line.offsetWidth;
-				
+        var shiftX = startX - moveEvent.clientX;
+				var leftLimit = 0;
+				var percent = 100;
+				var phobosMultiplier = 3;
+				var heatMultiplier = 3;
+							
 				if ((toggle.offsetLeft - shiftX) <= leftLimit) {
 					toggle.style.left = '0%';
 					bar.style.width = '0%';
-				} else if ((toggle.offsetLeft - shiftX) > rightLimit) {
+				} else if ((toggle.offsetLeft - shiftX) > line.offsetWidth) {
 					toggle.style.left = '100%';
 					bar.style.width = '100%';
 				} else {
-					toggle.style.left = (toggle.offsetLeft - shiftX) * 100 / 455 + '%';
-					bar.style.width = (toggle.offsetLeft - shiftX)* 100 / 455 + '%';
+					toggle.style.left = (toggle.offsetLeft - shiftX) * percent / line.offsetWidth + '%';
+					bar.style.width = (toggle.offsetLeft - shiftX) * percent / line.offsetWidth + '%';
 				}
 				
 				startX = moveEvent.clientX;
-				var effect = parseInt(toggle.style.left, 10) / 100;
+				var effect = parseInt(toggle.style.left, 10) / percent;
 				
 				if (photo.classList.contains('effect-chrome')) {
 					photo.style.filter = 'grayscale(' + effect + ')';
 				} else if (photo.classList.contains('effect-sepia')) {
 					photo.style.filter = 'sepia(' + effect + ')';
 				} else if (photo.classList.contains('effect-marvin')) {
-					photo.style.filter = 'invert(' + effect + ')';
+					photo.style.filter = 'invert(' + effect * percent + '%)';
 				} else if (photo.classList.contains('effect-phobos')) {
-					photo.style.filter = 'blur(' + effect + ')';
+					photo.style.filter = 'blur(' + effect * phobosMultiplier + 'px)';
 			  } else if (photo.classList.contains('effect-heat')) {
-					photo.style.filter = 'brightness(' + effect + ')';
-				} else {
-					toggle.style.display = 'none';
-				}
+					photo.style.filter = 'brightness(' + effect * heatMultiplier + ')';
+				} 
 			}
 			function onMouseUp(upEvent) {
 				upEvent.preventDefault();
@@ -196,6 +227,5 @@
 			  document.removeEventListener('mouseup', onMouseUp);				
 			}
 		}
-	}
-	
+	}	
 })();
