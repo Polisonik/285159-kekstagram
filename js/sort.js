44fa9aa2658a81161cc041e1e4dfacd8a25d53fe
Polisonik <allred@inbox.ru>
2017-09-15 00:01:39 +0300

    сортировка случайные, обсуждаемые

--- js/sort.js
+++ js/sort.js
@@ -2,7 +2,7 @@
 	window.sort = function(data) {
 		var array = data;
 		var filters = document.querySelector('.filters');
-		filters.addEventListener('change', onChange);
+		filters.addEventListener('click', onChange);
 		
 		function onChange(event) {
 			var target = event.target;
@@ -11,17 +11,22 @@
       };
 			if (target.id === 'filter-recommend') {
 				updatePictures(array);
-			}
-			
+			};		
 			if (target.id === 'filter-popular') {
-				var arrayOfPopular = array.slice().sort(compareLikes);
-				console.log(arrayOfPopular);
-				updatePictures(arrayOfPopular);
-			}
-			
+				updatePictures(array.slice().sort(compareLikes));
+			};
+			if (target.id === 'filter-random') {
+				updatePictures(array.slice().sort(compareRandom));
+			};
+			if (target.id === 'filter-discussed') {
+				updatePictures(array.slice().sort(compareLengthComments));
+			}			
 		}
 		function compareLikes (first,second) {
-			return (second.likes - first.likes )
+			return (second.likes - first.likes)
+		}
+		function compareLengthComments(first, second) {
+			return (second.comments.length - first.comments.length)
 		}
 		function updatePictures(newArray) {
 			var oldPictures = document.querySelectorAll('.picture');
@@ -30,6 +35,8 @@
       }
 			window.render(newArray);
 		}
-		
+		function compareRandom(a, b) {
+      return Math.random() - 0.5;
+    }		
 	}
 })();
