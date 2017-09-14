8d2579446c01cfa73415c3ae56df023e0638254c
Polisonik <allred@inbox.ru>
2017-09-14 23:05:41 +0300

    реализация сортировки рекомендуемые, популярные

--- js/gallery.js
+++ js/gallery.js
@@ -3,7 +3,8 @@
 (function () {
   // Добавление фотографий в блок .pictures
   function successHandler(arrayPictures) {
-    window.render(arrayPictures);
+		window.render(arrayPictures);
+		window.sort(arrayPictures);		
   }
   window.backend.load(successHandler, window.utils.showErrorsConnection);
 })();
--- js/sort.js
+++ js/sort.js
@@ -1,4 +1,35 @@
-'use strict';
-(function () {
-console.log (window.gallery());
+(function(){
+	window.sort = function(data) {
+		var array = data;
+		var filters = document.querySelector('.filters');
+		filters.addEventListener('change', onChange);
+		
+		function onChange(event) {
+			var target = event.target;
+			if (target.tagName !== 'INPUT') {
+        return;
+      };
+			if (target.id === 'filter-recommend') {
+				updatePictures(array);
+			}
+			
+			if (target.id === 'filter-popular') {
+				var arrayOfPopular = array.slice().sort(compareLikes);
+				console.log(arrayOfPopular);
+				updatePictures(arrayOfPopular);
+			}
+			
+		}
+		function compareLikes (first,second) {
+			return (second.likes - first.likes )
+		}
+		function updatePictures(newArray) {
+			var oldPictures = document.querySelectorAll('.picture');
+			for (var i = 0; i < oldPictures.length; i++) {
+        oldPictures[i].remove();
+      }
+			window.render(newArray);
+		}
+		
+	}
 })();
